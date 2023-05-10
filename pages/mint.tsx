d8735a929dceb7b929e8ce02d1585d22a01944cf
Dustin Turska <86588710+dturska12@users.noreply.github.com>
2023-05-09 20:14:48 -0500

    Update mint.tsx

--- pages/mint.tsx
+++ pages/mint.tsx
@@ -20,7 +20,7 @@ const Mint: NextPage = () => {
                     </Text>
                 </Text>
                 <iframe
-                    src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc1155.html?contract=0x7b4B550d6cbf55441f6153c71A5D173d860d83fE&chain=%7B%22name%22%3A%22Ethereum+Mainnet%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fethereum.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22eth%22%2C%22chainId%22%3A1%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22ethereum%22%7D&tokenId=3&theme=dark"
+                    src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc1155.html?contract=0x7b4B550d6cbf55441f6153c71A5D173d860d83fE&chain=%7B%22name%22%3A%22Ethereum+Mainnet%22%2C%22chain%22%3A%22ETH%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fethereum.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Ether%22%2C%22symbol%22%3A%22ETH%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22eth%22%2C%22chainId%22%3A1%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22ethereum%22%7D&tokenId=3&theme=dark&primaryColor=blue"
                     width="600px"
                     height="600px"
                 ></iframe>
@@ -38,7 +38,7 @@ const Mint: NextPage = () => {
                     Phoenix
                 </Text>
                 <iframe
-                    src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc1155.html?contract=0x7Bb78c39514d8d8720e380493ac367DD4D5b03A0&chain=%7B%22name%22%3A%22Phoenix+Mainnet%22%2C%22chain%22%3A%22Phoenix%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fphoenix.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Phoenix%22%2C%22symbol%22%3A%22PHX%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22Phoenix%22%2C%22chainId%22%3A13381%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22phoenix%22%7D&tokenId=0&theme=dark"
+                    src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc1155.html?contract=0x7Bb78c39514d8d8720e380493ac367DD4D5b03A0&chain=%7B%22name%22%3A%22Phoenix+Mainnet%22%2C%22chain%22%3A%22Phoenix%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fphoenix.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Phoenix%22%2C%22symbol%22%3A%22PHX%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22Phoenix%22%2C%22chainId%22%3A13381%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22phoenix%22%7D&tokenId=3&theme=dark&primaryColor=orange"
                     width="600px"
                     height="600px"
                 ></iframe>
@@ -47,7 +47,7 @@ const Mint: NextPage = () => {
                     Binance Smart Chain
                 </Text>
                 <iframe
-                    src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc1155.html?contract=0x83fc7f12b5CEafEd4c435A26ec1758EdA4A4d267&chain=%7B%22name%22%3A%22Binance+Smart+Chain+Mainnet%22%2C%22chain%22%3A%22BSC%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fbinance.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Binance+Chain+Native+Token%22%2C%22symbol%22%3A%22BNB%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22bnb%22%2C%22chainId%22%3A56%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22binance%22%7D&tokenId=0&theme=dark"
+                    src="https://ipfs.thirdwebcdn.com/ipfs/QmfK9mw9eQKE9vCbtZht9kygpkNWffdwibsJPnCo7MBN4M/erc1155.html?contract=0x83fc7f12b5CEafEd4c435A26ec1758EdA4A4d267&chain=%7B%22name%22%3A%22Binance+Smart+Chain+Mainnet%22%2C%22chain%22%3A%22BSC%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fbinance.rpc.thirdweb.com%2F5a9bc94b87f7cbbbfbbc234bf1e07f0adf5f3cf3012c9f26f9fc9820d64df93a%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Binance+Chain+Native+Token%22%2C%22symbol%22%3A%22BNB%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22bnb%22%2C%22chainId%22%3A56%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22binance%22%7D&tokenId=0&theme=dark&primaryColor=yellow"
                     width="600px"
                     height="600px"
                 ></iframe>
